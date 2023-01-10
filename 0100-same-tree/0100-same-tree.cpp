accbacfb61c7a49c1f3afaba8d517f2c8597314d
Nishchay007 <87924271+Nishchay007@users.noreply.github.com>
2023-01-10 11:03:52 +0530

    Time: 0 ms (100.00%), Space: 10 MB (79.93%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,13 +14,14 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL&&q==NULL)
             return true;
-        if((p==NULL&&q)||(q==NULL&&p))
+        else if(p==NULL||q==NULL)
             return false;
-            
-       if(p->val!=q->val)
-           return false;
-        return (isSameTree(p->left,q->left)&&isSameTree(p->right,q->right));
+         else if(p->val!=q->val)
+         return false;
+         bool x=isSameTree(p->left,q->left);
+            bool y= isSameTree(p->right,q->right);
         
+        return x&&y;
         
     }
 };
