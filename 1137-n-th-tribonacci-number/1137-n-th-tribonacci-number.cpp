fdc216b3dc717f134286ca7621d2c5b01d1716d7
Nishchay007 <87924271+Nishchay007@users.noreply.github.com>
2023-01-30 12:00:23 +0530

    Time: 4 ms (10.94%), Space: 6.1 MB (22.73%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,11 +4,10 @@ public:
         vector<long long int>dp(n+1,0);
        
         if(n==0)
-            return 0;
-        else if(n==1||n==2)
-            return 1;
-        else
-        {
+        return 0;
+      if(n==1||n==2)
+        return 1;
+       
         dp[0]=0;
         dp[1]=1;
         dp[2]=1;
@@ -16,6 +15,6 @@ public:
             dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
         }
         return dp[n];
-        }
+
     }
 };
